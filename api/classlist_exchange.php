cbc6d2c8279f9435969d9d677b507cef95135fa4
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-04 20:23:46 +0930

    classlist example skeleton

---
+++ api/classlist_exchange.php
@@ -0,0 +1,11 @@
+<?php
+
+require "authentication_header.fnc.php";
+
+$array = array( array("id" => "1", 
+		"classcode" => "8883cc34"), 
+		array("id" => "2", 
+		"email" => "828f2c23"), );
+
+
+echo json_encode($array);
