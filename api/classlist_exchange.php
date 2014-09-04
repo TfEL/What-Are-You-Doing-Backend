703e8385f05581bee2290b8c5c3d521426204b8c
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-04 20:37:00 +0930

    Formatting issue

--- api/classlist_exchange.php
+++ api/classlist_exchange.php
@@ -5,7 +5,7 @@ require "authentication_header.fnc.php";
 $array = array( array("id" => "1", 
 		"classcode" => "8883cc34"), 
 		array("id" => "2", 
-		"email" => "828f2c23"), );
+		"classcode" => "828f2c23"), );
 
 
 echo json_encode($array);
