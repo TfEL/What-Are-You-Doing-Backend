5d74f2a6ea3725339b8d883d06f435a5240cc83e
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-04 17:19:34 +0930

    Oops, forgot php

--- api/teacherslist_exchange.php
+++ api/teacherslist_exchange.php
@@ -1,3 +1,5 @@
+<?php
+
 require "authentication_header.fnc.php";
 
 echo json_encode(array("id" => "1", "email" => "example@example.com", "password" => "example", "id" => "2", "email" => "example2@example.com", "password" => "password2", ));
