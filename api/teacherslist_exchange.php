789a19c913b9679bef2cc121a89937724617595e
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-04 18:07:57 +0930

    identifierless

--- api/teacherslist_exchange.php
+++ api/teacherslist_exchange.php
@@ -2,10 +2,10 @@
 
 require "authentication_header.fnc.php";
 
-$array = array( 1 => array("id" => "1", 
+$array = array( array("id" => "1", 
 		"email" => "example@example.com",
 		"password" => "example"), 
-		2 => array("id" => "2", 
+		array("id" => "2", 
 		"email" => "example2@example.com", 
 		"password" => "password2"), );
 
