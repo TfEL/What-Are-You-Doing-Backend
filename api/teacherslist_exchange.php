cbeb6e539722da1d3f3657a4e223369f65fb8f94
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-04 17:18:35 +0930

    teacher exchange example

---
+++ api/teacherslist_exchange.php
@@ -0,0 +1,3 @@
+require "authentication_header.fnc.php";
+
+echo json_encode(array("id" => "1", "email" => "example@example.com", "password" => "example", "id" => "2", "email" => "example2@example.com", "password" => "password2", ));
