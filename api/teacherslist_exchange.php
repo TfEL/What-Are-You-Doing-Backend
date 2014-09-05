e65ff628ace212ee9c690bd8d2c7c1d5a5c24bbf
Aidan Cornelius-Bell <aidan@acproduction.com.au>
2014-09-05 09:36:03 +0930

    remove extra 'stuff'

--- api/teacherslist_exchange.php
+++ api/teacherslist_exchange.php
@@ -55,16 +55,3 @@ foreach ($result as $key => $value) {
 echo json_encode($array);
 
 ?>
-
-
-require "authentication_header.fnc.php";
-
-$array = array( array("id" => "1", 
-		"email" => "example@example.com",
-		"password" => "example"), 
-		array("id" => "2", 
-		"email" => "example2@example.com", 
-		"password" => "password2"), );
-
-
-echo json_encode($array);
